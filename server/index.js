e7b386e3d9e1e095bda13ae0886b2a5315dae047
Kenny <Kennyytran95@gmail.com>
2022-06-11 01:53:52 -0700

    api rough draft completed

--- server/index.js
+++ server/index.js
@@ -9,21 +9,31 @@ app.use(express.json());
 app.get("/products", (req, res) => {
   let count = req.query.count || 5;
   let page = req.query.page || 1;
-  queries(count, page)
+  queries
+    .getProducts(count, page)
     .then((result) => res.status(200).send(result.rows))
     .catch((err) => console.error(err));
 });
 
-app.get("/products/:id", (req, res) => {
-  console.log("hello products id");
+app.get("/products/:product_id", (req, res) => {
+  queries
+    .getProductsById(req.params.product_id)
+    .then((result) => res.status(200).send(result.rows[0]))
+    .catch((err) => console.error(err));
 });
 
-app.get("/products/:id/styles", (req, res) => {
-  console.log("hello product id styles");
+app.get("/products/:product_id/styles", (req, res) => {
+  queries
+    .getStylesById(req.params.product_id)
+    .then((result) => res.status(200).send(result.rows[0]))
+    .catch((err) => console.error(err));
 });
 
-app.get("/products/:id/related", (req, res) => {
-  console.log("hello products id related");
+app.get("/products/:product_id/related", (req, res) => {
+  queries
+    .getRelatedById(req.params.product_id)
+    .then((result) => res.status(200).send(result.rows[0]))
+    .catch((err) => console.error(err));
 });
 
 const port = 3000;
--- server/queries.js
+++ server/queries.js
@@ -4,4 +4,21 @@ const getProducts = (count, page) => {
   return pool.query(`SELECT * FROM products ORDER BY id LIMIT ${count}`);
 };
 
-module.exports = getProducts;
+const getProductsById = (product_id) => {
+  return pool.query(`SELECT * FROM products WHERE id=${product_id}`);
+};
+
+const getStylesById = (product_id) => {
+  return pool.query(`SELECT * FROM styles WHERE id=${product_id}`);
+};
+
+const getRelatedById = (product_id) => {
+  return pool.query(`SELECT * FROM related WHERE id=${product_id}`);
+};
+
+module.exports = {
+  getProducts,
+  getProductsById,
+  getStylesById,
+  getRelatedById,
+};
